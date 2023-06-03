123f3caf0227f45673021e81513fba0ea8027ef2
tornikee <tornikexoxiashvili@gmail.com>
2023-06-03 23:16:31 +0400

    change

--- src/main/java/com/epam/rd/autotasks/SortingTest.java
+++ src/main/java/com/epam/rd/autotasks/SortingTest.java
@@ -2,6 +2,8 @@ package com.epam.rd.autotasks;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 
@@ -31,37 +33,16 @@ public class SortingTest {
 
     @Test
     public void testSortedArraysCase() {
-        int[] array1 = {1, 2, 3, 4, 5};
+        int[] array1 = {1, 2, 4, 5, 8};
         sorting.sort(array1);
-        assertArrayEquals(new int[]{1, 2, 3, 4, 5}, array1);
+        assertArrayEquals(new int[]{1, 2, 4, 5, 8}, array1);
 
-        int[] array2 = {10, 20, 30, 40, 50};
-        sorting.sort(array2);
-        assertArrayEquals(new int[]{10, 20, 30, 40, 50}, array2);
-
-        int[] array3 = {-5, -4, -3, -2, -1};
-        sorting.sort(array3);
-        assertArrayEquals(new int[]{-5, -4, -3, -2, -1}, array3);
     }
 
     @Test
     public void testOtherCases() {
-        int[] array1 = {5, 2, 8, 1, 4}; // Unsorted array
-        sorting.sort(array1);
-
-        // Assert that the array is sorted in ascending order
-        assertArrayEquals(new int[]{1, 2, 4, 5, 8}, array1);
-
-        int[] array2 = {9, 2, 5, 1, 6, 3}; // Unsorted array
-        sorting.sort(array2);
-
-        // Assert that the array is sorted in ascending order
-        assertArrayEquals(new int[]{1, 2, 3, 5, 6, 9}, array2);
-
-        int[] array3 = {10, -5, 0, -1, 8}; // Unsorted array with negative numbers
+        int[] array3 = {10, -5, 0, -1, 8};
         sorting.sort(array3);
-
-        // Assert that the array is sorted in ascending order
         assertArrayEquals(new int[]{-5, -1, 0, 8, 10}, array3);
     }
 
