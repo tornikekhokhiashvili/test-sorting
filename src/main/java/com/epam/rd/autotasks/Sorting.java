a61c742ab73bcccbc2870c6b36b37a8e3ed6d8e8
tornikekhokhiashvili <tornikexoxiashvili@gmail.com>
2024-06-03 23:13:03 +0400

    Initial Commit

--- src/main/java/com/epam/rd/autotasks/Sorting.java
+++ src/main/java/com/epam/rd/autotasks/Sorting.java
@@ -6,18 +6,6 @@ public class Sorting {
     public void sort(int[] array){
         if(array == null)
             throw new IllegalArgumentException("Given array is null");
-
-//        int n=array.length;
-//        for (int i=0;i<n-1;i++){
-//            for (int j=0;j<n-i-1;j++){
-//                if (array[j] > array[j + 1]) {
-//                    int temp = array[j];
-//                    array[j] = array[j + 1];
-//                    array[j + 1] = temp;
-//                }
-//            }
-//        }
         Arrays.sort(array);
-
     }
 }
--- src/main/java/com/epam/rd/autotasks/SortingTest.java
+++ src/main/java/com/epam/rd/autotasks/SortingTest.java
@@ -1,11 +1,6 @@
 
 package com.epam.rd.autotasks;
-
-import junit.framework.AssertionFailedError;
 import org.junit.Test;
-
-import java.util.Arrays;
-
 import static org.junit.Assert.*;
 
 
@@ -26,10 +21,8 @@ public class SortingTest {
         assertEquals(0, array.length);
     }
 
-
     @Test
     public void testSingleElementArrayCase() {
-
         int[] array = {4};
         sorting.sort(array);
         assertEquals(1, array.length);
@@ -42,24 +35,14 @@ public class SortingTest {
         sorting.sort(array);
         int[] expected = { 2, 3, 4, 5};
         assertArrayEquals(expected, array);
-
     }
 
     @Test
     public void testOtherCases() {
-//        int[] array = {5, 2, 9,1,3};
-//        sorting.sort(array);
-//        int[] expected = { 1,2, 3, 5, 9};
-//        assertArrayEquals(expected, expected);
         int[] array = {5, 2, 4, 1, 3};
-        int[] expectedSortedArr = {1, 2, 3, 4, 5};
-
+        int[] expectedSortedArray = {1, 2, 3, 4, 5};
         sorting.sort(array);
-
-        // Assert that the sorted array matches the expected sorted array
-        assertArrayEquals(expectedSortedArr, array);
-
+        assertArrayEquals(expectedSortedArray, array);
     }
-
 }
 
